6b957e93c7b5f3f5dff4c84f587ff69ec2324a10
Loïc <ldivad@isep.fr>
2016-02-15 23:26:15 +0100

    Add 1st lab of A2

---
+++ Labone/src/main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class main {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> items = new ArrayList<String>(); 
+		items.add("a");
+		items.add("b");
+		items.add("c");
+		items.add("s");
+
+		for(String i : items){
+			System.out.print(i);
+		}
+	}
+
+}
